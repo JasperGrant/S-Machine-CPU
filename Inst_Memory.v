47cf8230d456e82161d3dd2a42a19fa107783a67
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2023-11-07 19:32:27 -0400

    Reorganized Repo

--- Inst_Memory.v
+++ Inst_Memory.v
@@ -11,8 +11,14 @@ module InstMemory(
 
     reg [15:0] memory [255:0];
     //Where we define our code
-    memory[0] = 16'b0100000000000000;
-    memory[1] = 16'b0100000000000000;
+    memory[0] = 16'b0000010000000001;
+    memory[1] = 16'b0000110000000001;
+    memory[2] = 16'b0100000000000000;
+    memory[3] = 16'b0101000000000000;
+    memory[4] = 16'b0010100000000001;
+    memory[5] = 16'b0110000000000000;
+    memory[6] = 16'b0111000000000000;
+    memory[7] = 16'b1000000000000000;
 
     always@(posedge clk, addr) begin
         data_out = memory[addr];
